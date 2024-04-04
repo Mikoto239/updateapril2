89eb8002b466110a5e075cb29b621b85a13cf78f
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-04-04 13:40:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -39,13 +39,12 @@ app.get('/getlocation', async (req, res) => {
       return res.status(404).json({ message: 'No information found' });
     }
 
-   return res.status(200).json({ latitude: result.latitude });
+    return res.status(200).json(results);
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: 'Internal server error' });
   }
 });
-
 app.post('/data', (req, res) => {
   const { vibrationDuration, latitude, longitude, uniqueId } = req.body; // Updated to include macAddress
 
