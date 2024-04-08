9816436f02bf87323fd2e9f77bb21c0d53748c48
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-04-08 10:20:32 +0800

    Update app.js

--- app.js
+++ app.js
@@ -136,8 +136,8 @@ const { uniqueId } = req.body; // Extract the uniqueId from the request body
 });
 
 
-app.get('/usernumber', async (req, res) => {
-  const { uniqueId } = req.query;
+app.post('/usernumber', async (req, res) => {
+  const { uniqueId } = req.body;
   
   try {
     const user = await User.findOne({ uniqueId });
