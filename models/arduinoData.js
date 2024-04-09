574d7294dc591ed836d10a77a242cb0465da490e
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-04-10 00:25:29 +0800

    Update arduinoData.js

--- models/arduinoData.js
+++ models/arduinoData.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
-const timezone = require('moment-timezone');
+const moment = require('moment-timezone');
 
-timezone.tz.setDefault('Asia/Manila');
+// Set default timezone
+moment.tz.setDefault('Asia/Manila');
 
 const arduinoDataSchema = new mongoose.Schema({
   vibrationDuration: Number,
@@ -10,12 +11,10 @@ const arduinoDataSchema = new mongoose.Schema({
   uniqueId: String,
   vibrateAt: {
     type: Date,
-    default: () => moment.tz('Asia/Manila').add(8, 'hours').toDate() 
+    default: () => moment().toDate() // Use moment() to get current time
   }
 });
-
 
 const ArduinoData = mongoose.model('ArduinoData', arduinoDataSchema);
 
 module.exports = ArduinoData;
-
