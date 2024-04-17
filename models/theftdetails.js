ca29191d94b7ecc8accefc06f324518978655c42
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-04-17 21:53:34 +0800

    Update theftdetails.js

--- models/theftdetails.js
+++ models/theftdetails.js
@@ -4,7 +4,7 @@ const moment = require('moment-timezone');
 moment.tz.setDefault('Asia/Manila');
 
 const theftdetailsschema = new mongoose.Schema({
-  uniqueId: { type: String }, // No unique constraint specified
+  as: { type: String }, // No unique constraint specified
   happenedAt: {
     type: Date,
     default: () => moment.tz('Asia/Manila').add(8, 'hours').toDate() 
