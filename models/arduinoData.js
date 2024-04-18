7a98f6e62368cb61d52c278448abc71d4c96ee1a
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-04-18 12:20:52 +0800

    Update arduinoData.js

--- models/arduinoData.js
+++ models/arduinoData.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
-
-// Define the schema
-const arduinoDataSchema = new mongoose.Schema({
-  vibrationDuration: String,
-  latitude: Number,
-  longitude: Number,
-  uniqueId: String,
-  CreatedAt: {
-    type: Date,
-        default: () => moment.tz('Asia/Manila').add(8, 'hours').toDate() 
-  },
-});
-
-// Create the model
-const ArduinoData = mongoose.model('ArduinoData', arduinoDataSchema);
-
-module.exports = ArduinoData;
+  const mongoose = require('mongoose');
+  
+  // Define the schema
+  const arduinoDataSchema = new mongoose.Schema({
+    vibrationDuration: String,
+    latitude: Number,
+    longitude: Number,
+    uniqueId: String,
+    CreatedAt: {
+      type: Date,
+          default: () => moment.tz('Asia/Manila').add(8, 'hours').toDate() 
+    }
+  });
+  
+  // Create the model
+  const ArduinoData = mongoose.model('ArduinoData', arduinoDataSchema);
+  
+  module.exports = ArduinoData;
